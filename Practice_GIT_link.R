17afa904287884955da224c9d6a6de7a4f53d035
Atchuta Raviteja <41302889+RAVITeja2394@users.noreply.github.com>
2019-03-22 13:00:52 +0530

    Practice Git link ffile gets uploaded
    Created a new R script file under existing R-Project. Trying to link or show up the file in GIT HUB repository

---
+++ Practice_GIT_link.R
@@ -0,0 +1,11 @@
+Print('Welcome Ravi!')
+
+Print("U'r Exploring Git-Hub and R-Version control")
+
+print("First in this lesson U vl learn linking R script to Git interface by following below steps:")
+
+print("Step1:")
+
+print("First Create new project in R-Studio, if project is already existing then create a new file
+      in same project")
+
